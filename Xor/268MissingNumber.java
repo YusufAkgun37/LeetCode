ae122a239aa0a594b4f1638476d84e70b979279b
Yusuf Akgun <yusufakgun1050@gmail.com>
2026-02-07 01:00:31 +0300

    Add solution for Single Number problem

---
+++ Xor/268MissingNumber.java
@@ -0,0 +1,12 @@
+package Xor;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        int xor = 0;
+        for(int num : nums){
+            xor ^= num;
+        }
+        return xor;
+    }
+}
