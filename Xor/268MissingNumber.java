57cf61bebd791256e6c654459d7577eaea3e5313
Yusuf Akgun <yusufakgun1050@gmail.com>
2026-02-07 01:22:39 +0300

    Refactor missingNumber solution to use XOR method

--- Xor/268MissingNumber.java
+++ Xor/268MissingNumber.java
@@ -1,12 +1,29 @@
 package Xor;
 
 class Solution {
-    public int singleNumber(int[] nums) {
+    public int missingNumber(int[] nums) {
+        int xor = nums.length;
 
-        int xor = 0;
-        for(int num : nums){
-            xor ^= num;
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= i;
+            xor ^= nums[i];
         }
+
         return xor;
     }
 }
+/* 
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+}
+*/
