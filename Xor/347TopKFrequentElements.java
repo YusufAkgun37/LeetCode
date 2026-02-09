5f00b6e5db3130472e43632331b590a903aa48f8
Yusuf Akgun <yusufakgun1050@gmail.com>
2026-02-09 23:35:57 +0300

    Add solution for Top K Frequent Elements problem

---
+++ Xor/347TopKFrequentElements.java
@@ -0,0 +1,42 @@
+package Xor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        // 1️⃣ Frekans say
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        // 2️⃣ Bucket oluştur
+        List<Integer>[] buckets = new List[nums.length + 1];
+        for (int key : freqMap.keySet()) {
+            int freq = freqMap.get(key);
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(key);
+        }
+
+        // 3️⃣ En sık k taneyi topla
+        int[] result = new int[k];
+        int idx = 0;
+
+        for (int i = buckets.length - 1; i >= 0 && idx < k; i--) {
+            if (buckets[i] != null) {
+                for (int num : buckets[i]) {
+                    result[idx++] = num;
+                    if (idx == k) break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
